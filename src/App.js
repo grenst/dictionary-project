0b6c04ea9225c15f502f199d82b9b105c96ed893
grenst <74575794+grenst@users.noreply.github.com>
2022-10-16 14:37:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a href="/" className="text-warning">
-          dfg
+      <header className="App-header text-center display-2">
+        <a href="/" className="text-success text-decoration-none">
+          TEST Hello
         </a>
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
