d8dbba57974647fc11bdcbc01f3ffc625aca7725
grenst <74575794+grenst@users.noreply.github.com>
2022-10-16 14:26:27 +0200

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/" className="text-warning">
+          dfg
+        </a>
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
